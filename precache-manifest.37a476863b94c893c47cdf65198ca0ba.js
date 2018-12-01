daa618272adfc63f4db99ac28afcf616c28ebb74
Aleksey Dibrivniy <a.dibrivniy@rambler-co.ru>
2018-12-01 21:29:56 +0300

    Updates

---
+++ precache-manifest.37a476863b94c893c47cdf65198ca0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app-v2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb38ca47e98dd0a16f10",
+    "url": "/create-react-app-v2/static/js/runtime~main.cb38ca47.js"
+  },
+  {
+    "revision": "58a574bbe2cc6fb5227c",
+    "url": "/create-react-app-v2/static/js/main.58a574bb.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/create-react-app-v2/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "58a574bbe2cc6fb5227c",
+    "url": "/create-react-app-v2/static/css/main.4a655324.chunk.css"
+  },
+  {
+    "revision": "1b8ca8d79597cba276ec611ed5d92db8",
+    "url": "/create-react-app-v2/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/create-react-app-v2/precache-manifest.37a476863b94c893c47cdf65198ca0ba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/create-react-app-v2/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
